13a5e7b76849b30e73e00ef09821214a028731b1
Jim Lake <jim@blueskylabs.com>
2018-10-17 08:48:43 -0700

    skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2015
+  },
+  "rules": {
+    "indent": 0,
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": 0
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# steamship
+# harbour-server
+
